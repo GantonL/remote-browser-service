844035d499248aa621934112e76337204a70b781
Guy Lahav <guylahav@machine.local>
2025-12-25 22:17:24 +0200

    feat: initial base deno project with danet

---
+++ src/bootstrap.ts
@@ -0,0 +1,20 @@
+import "@std/dotenv/load";
+import { AppModule } from "./app.module.ts";
+import { DanetApplication } from "@danet/core";
+import { loggerMiddleware } from "./logger.middleware.ts";
+import { SpecBuilder, SwaggerModule } from "@danet/swagger";
+export const bootstrap = async () => {
+  const application = new DanetApplication();
+  await application.init(AppModule);
+  const spec = new SpecBuilder()
+    .setTitle("Remote Browser Service")
+    .setDescription(
+      "Generate snapshots, pdfs and automations in remote browsers",
+    )
+    .setVersion("1.0")
+    .build();
+  const document = await SwaggerModule.createDocument(application, spec);
+  await SwaggerModule.setup("api", application, document);
+  application.addGlobalMiddlewares(loggerMiddleware);
+  return application;
+};
